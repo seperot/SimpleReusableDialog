99be6a9aa265e477525ce8b11d054062090e19e5
Ian Hayward <ian.hayward@havasww.com>
2020-04-08 21:45:11 +0100

    Missing File from last merge

--- reusabledialog/src/main/java/dev/ijh/reusabledialog/ReusableDialog.kt
+++ reusabledialog/src/main/java/dev/ijh/reusabledialog/ReusableDialog.kt
@@ -1,5 +1,6 @@
-package dev.ijh.simplereusabledialog
+package dev.ijh.reusabledialog
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.*
 import android.widget.Button
@@ -9,15 +10,11 @@ import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.fragment_reusable_dialog.*
 import java.util.*
 
-class ReusableDialog : DialogFragment() {
+class ReusableDialog : DialogFragment(){
 
     private var title: String? = null
     private var infoText: String? = null
     private var dialogButtonsData: ArrayList<ReusableDialogButtonData>? = null
-
-    interface ReusableDialogListenerSingle {
-        fun onDialogConfirmClick(dialogFragment: DialogFragment, index: Int)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_reusable_dialog, container, false)
@@ -32,6 +29,9 @@ class ReusableDialog : DialogFragment() {
     override fun onResume() {
         super.onResume()
         dialog?.window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        dialog.setOnCancelListener {
+            dialogButtons.clear()
+        }
     }
 
     private fun setText() {
@@ -71,47 +71,60 @@ class ReusableDialog : DialogFragment() {
                 context?.resources?.let { ResourcesCompat.getColor(it, buttonData.textColor, null) }?.let { btnOption.setTextColor(it) }
                 btnOption.background = resources.getDrawable(buttonData.buttonBg, null)
                 btnOption.isAllCaps = false
-                btnOption.setOnClickListener { buttonData.reusableDialogListener.onDialogConfirmClick(dialog, position) }
+                btnOption.setOnClickListener {
+                    buttonData.reusableDialogListener.onDialogButtonClick(dialog, position)
+                    dialogButtons.clear()
+                    edgeClickDisabled = true
+                }
                 llButtonArea?.addView(btnOption)
             }
         }
     }
 
-    /**
-     * General construct for ReusableDialogButtonData
-     *
-     * @param buttonTitle            - The Button Title
-     * @param reusableDialogListener - The button listener
-     * @param buttonBg               - The button color [.FIRST_BUTTON_BG_DRAWABLE], [.SECOND_BUTTON_BG_DRAWABLE], ETC
-     * @param textColor              - The button text color [.FIRST_BUTTON_DEFAULT_TEXT_COLOR], [.SECOND_BUTTON_DEFAULT_TEXT_COLOR], ETC
-     */
-    class ReusableDialogButtonData(var buttonTitle: String,
-                                   var reusableDialogListener: ReusableDialogListenerSingle,
-                                   var buttonBg: Int, var textColor: Int) {
-        companion object {
-            const val FIRST_BUTTON_DEFAULT_TEXT_COLOR = android.R.color.holo_blue_bright
-            const val SECOND_BUTTON_DEFAULT_TEXT_COLOR = android.R.color.white
-            const val FIRST_BUTTON_BG_DRAWABLE = R.drawable.button_primary
-            const val SECOND_BUTTON_BG_DRAWABLE = R.drawable.button_secondary
-        }
-    }
+    class ReusableDialogButtonData(var buttonTitle: String, var buttonBg: Int, var textColor: Int, var reusableDialogListener: ReusableDialogListener)
 
     companion object {
+
+        private var dialogButtons = ArrayList<ReusableDialogButtonData>()
+        private var edgeClickDisabled = true
+
+        //TODO: Add fun for setting custom margin, custom padding and an icon at the top
+
+        /**
+         * Switch to allow the user to exit dialog by hitting the back button or click off the dialog area
+         * @param isCancelable            - True = Allowed to Cancel, False = Unable to Cancel
+         */
+        fun dialogCancelable(isCancelable: Boolean) {
+            edgeClickDisabled = isCancelable
+        }
+
+        /**
+         * General construct for RegisterButton
+         * @param buttonTitle            - The Button Title
+         * @param dialogButtonListener - The button onClickListener
+         * @param buttonBackground               - The button background color: R.color or android.R.color work
+         * @param textColor              - The button text color: R.color or android.R.color work
+         */
+        fun registerButton(buttonTitle: String, buttonBackground: Int, textColor: Int, dialogButtonListener: ReusableDialogListener) {
+            dialogButtons.add(ReusableDialogButtonData(buttonTitle, buttonBackground, textColor, dialogButtonListener))
+        }
+
         /**
          * Create ReusableDialog instance
-         *
          * @param title    - The dialog title
          * @param infoText - The dialog Content message
          * @param data     - the dialog buttons Array [ReusableDialogButtonData]
          * @return the ReusableDialog instance
          */
-        fun createDialogInstance(title: String?, infoText: String?, data: ArrayList<ReusableDialogButtonData>?): ReusableDialog {
+        fun createDialogInstance(title: String?,
+                                 infoText: String?): ReusableDialog {
             val dialog = ReusableDialog()
             val args = Bundle()
             dialog.title = title
             dialog.infoText = infoText
             dialog.arguments = args
-            dialog.dialogButtonsData = data
+            dialog.isCancelable = edgeClickDisabled
+            dialog.dialogButtonsData = dialogButtons
             return dialog
         }
     }
